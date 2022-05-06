30f29443d34fe579bd25abcc7286e102dc5aecac
Kingsley Atuba <atubak40@gmail.com>
2022-05-06 21:33:14 +0100

    Create  and implement the deposit function

--- Wallet/functions.py
+++ Wallet/functions.py
@@ -4,12 +4,13 @@ def create(email, firstname, lastname):
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
     c.execute("""CREATE TABLE IF NOT EXISTS customers(
-    email text unique,
-    first_name text,
-    last_name text
+    Email text unique,
+    First_name text,
+    Last_name text,
+    Balance integer 
     )""")
     try:
-        c.execute("INSERT INTO customers VALUES (?,?,?)", (email, firstname, lastname))
+        c.execute("INSERT INTO customers VALUES (?,?,?,?)", (email, firstname, lastname, 0))
     except:
         print(f"Sorry, {email} is already taken!!")
     c.execute("SELECT rowid, * FROM customers")
@@ -18,4 +19,19 @@ def create(email, firstname, lastname):
         print(item)
     conn.commit()
     conn.close()
+
+
+def deposit(email, amount):
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM customers WHERE Email = ?", (email,))
+    items = c.fetchall()
+    for item in items:
+        new_amount = (item[3] + amount)
+    c.execute("UPDATE customers SET Balance = ? WHERE Email = ?", (new_amount,email))
+    conn.commit()
+    conn.close()
+    print(f"Transaction successfull. You deposited {amount} Naira into your account!!")
+    print(f"Your new balance is {new_amount} Naira.")
+
         
--- Wallet/wallet.py
+++ Wallet/wallet.py
@@ -27,11 +27,26 @@ if __name__ == "__main__":
         help="fill your second name"
         )
 
+    parser.add_argument(
+        "--user",
+        type=str,
+        help="email to receive deposit"
+        )
+
+    parser.add_argument(
+        "--amount",
+        type=int,
+        help="amount to be deposited or transfered"
+        )
+
     args = parser.parse_args()
 
 
     if args.function == "create":
         functions.create(args.email, args.firstname, args.lastname)
+
+    elif args.function == "deposit":
+        functions.deposit(args.email, args.amount)
 
     
 
