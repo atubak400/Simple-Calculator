db32494b32df2c694f3465bc49b00216a6583e58
Kingsley Atuba <atubak40@gmail.com>
2022-05-07 13:42:28 +0100

    Added datetime module in my functions and Created_at and Update_at columns in the database

--- Wallet/functions.py
+++ Wallet/functions.py
@@ -1,16 +1,22 @@
 import sqlite3
+import datetime
+
+now = datetime.datetime.now()
 
 def create(email, firstname, lastname):
+    created_at = (now.strftime("%y-%m-%d, %H:%M:%S"))
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
     c.execute("""CREATE TABLE IF NOT EXISTS customers(
     Email text unique,
     First_name text,
     Last_name text,
-    Balance integer 
+    Balance integer,
+    Created_at integer,
+    Updated_at integer
     )""")
     try:
-        c.execute("INSERT INTO customers VALUES (?,?,?,?)", (email, firstname, lastname, 0))
+        c.execute("INSERT INTO customers VALUES (?,?,?,?,?,?)", (email, firstname, lastname, 0, created_at, "-"))
     except:
         print(f"Sorry, {email} is already taken!!")
     c.execute("SELECT rowid, * FROM customers")
@@ -22,13 +28,14 @@ def create(email, firstname, lastname):
 
 
 def deposit(email, amount):
+    updated_at = (now.strftime("%y-%m-%d, %H:%M:%S"))
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
     c.execute("SELECT * FROM customers WHERE Email = ?", (email,))
     items = c.fetchall()
     for item in items:
         new_amount = (item[3] + amount)
-    c.execute("UPDATE customers SET Balance = ? WHERE Email = ?", (new_amount,email))
+    c.execute("UPDATE customers SET Balance = ?, Updated_at = ? WHERE Email = ?", (new_amount, updated_at,email))
     conn.commit()
     conn.close()
     print(f"Transaction successfull. You deposited {amount} Naira into your account!!")
